bb9b23c4e3fca0115cd709bb67b26f94fffc7aca
Austin Bales <austin@manymoon.com>
2011-06-10 14:21:43 -0700

    Better packaging and ping support via postmark.

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 # poor man's reloader.
 # re-starts evilbot when he dies of an error.
 
-until /usr/bin/env coffee evilbot.coffee; do
-    echo "evilbot crashed with exit code $?. respawning.." >&2
+until /usr/bin/env coffee cylon.coffee; do
+    echo "Cylon crashed with exit code $?. respawning.." >&2
     sleep 1
 done
